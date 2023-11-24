4a662812deed58f3195924305ce408c4d246d1a9
developer-hajun <juni8828@naver.com>
2023-11-24 19:55:44 +0900

    큰수의 법칙

---
+++ 숫자 카드 게임.py
@@ -0,0 +1,9 @@
+n,m = map(int,input().split())
+
+max_result = -1
+for i in range(n):
+    data = list(map(int,input().split()))
+    min_value = min(data)
+    max_result = max(max_result,min_value)
+
+print(max_result)
