1b642ab3cf0498ac9bb5bcd62ffbdfc80adc6236
developer-hajun <juni8828@naver.com>
2024-08-10 18:26:00 +0900

    크게만들기

--- Cubeditor.py
+++ Cubeditor.py
@@ -1,22 +1,16 @@
-import sys
-def pa(pt):
-    F=[0]*len(pt)
-    F[0] = 0
-    i = 1
-    j = 0
-    while i<len(pt):
-        if pt[i] == pt[j]:
-            F[i] = j + 1
-            i +=1
-            j +=1
-        elif j > 0:
-            j = F[j - 1]
-        else:
-            F[j] = 0
-            i+=1
-    return F
-pat = sys.stdin.readline().rstrip()
-ans = 0
-for i in range(len(pat)):
-    ans = max(ans, max(pa(pat[i:])))
-print(ans)
+n, m = map(int, input().split())
+a = list(input())
+
+for i in range(n):
+    a[i] = int(a[i])
+answer = []
+count = 0
+for i in a:
+    while len(answer)>0 and count<m and answer[-1]<i:
+        answer.pop()
+        count+=1
+    answer.append(i)
+
+while len(answer)>(n-m):
+    answer.pop()
+print(''.join(map(str, answer)))
