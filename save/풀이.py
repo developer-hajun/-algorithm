47c7601441ef2d8e1bb29abd8f6e757ca262f283
developer-hajun <juni8828@naver.com>
2024-08-31 20:19:05 +0900

    낚시왕 -완료

--- save/풀이.py
+++ save/풀이.py
@@ -1,66 +1,82 @@
-import sys
+import heapq
 from collections import deque
-import copy
-input = sys.stdin.readline
-n=int(input())
+
+
+def find_shark(x):
+    global answer
+    now = 0
+    while True:
+        if matrix[now][x]:
+            answer += -(heapq.heappop(matrix[now][x])[0])
+            break
+        now += 1
+        if now>=r:
+            break
+
+
+def move_shark():
+    push=[]
+    for y in range(r):
+        for x in range(c):
+            if not matrix[y][x]:
+                continue
+            size, speed, moves = heapq.heappop(matrix[y][x])
+            my, mx,speed2 = y, x,speed
+            if speed==0:
+                push.append([my,mx,size,speed,moves])
+
+            while speed2 != 0:
+                ny = my + (move[moves][0] * speed2)
+                nx = mx + (move[moves][1] * speed2)
+                if ny<0:
+                    speed2 = abs(ny)
+                    my = 0
+                    moves = 2
+                elif ny>=r:
+                    speed2 = speed2 - (r - 1 - my)
+                    my = r - 1
+                    moves = 1
+                elif nx<0:
+                    speed2 = abs(nx)
+                    mx=0
+                    moves=3
+                elif nx>=c:
+                    speed2 = speed2 - (c-1-mx)
+                    mx = c-1
+                    moves = 4
+                else:
+                    #heapq.heappush(matrix[ny][nx],[size,speed,moves])
+                    push.append([ny,nx,size,speed,moves])
+                    break
+    for ny,nx,size,speed,mv in push:
+        if matrix[ny][nx]:
+            size2,speed2,mv2 = heapq.heappop(matrix[ny][nx])
+            if size<size2:
+                heapq.heappush(matrix[ny][nx],[size,speed,mv])
+            else:
+                heapq.heappush(matrix[ny][nx],[size2,speed2,mv2])
+        else:
+            heapq.heappush(matrix[ny][nx],[size,speed,mv])
+
+
+r, c, m = map(int, input().split())
+
 matrix = []
-now = []
-for _ in range(n):
-    line = list(map(int, input().split()))
-    for k in range(n):
-        if line[k]==9:
-            now = [_,k]
-            line[k]=0
+for i in range(r):
+    line = []
+    for j in range(c):
+        line2 = []
+        line.append(line2)
     matrix.append(line)
 
-
-shark_size = 2
-eat_fish = 0
-time=0
-def bfs():
-    queue = deque()
-    queue.append(now)
-    visit= [[-1]*n for _ in range(n)]
-    visit[now[0]][now[1]]=0
-    while queue:
-        y,x = queue.popleft()
-        for nextY,nextX in [-1,0],[1,0],[0,-1],[0,1]:
-            ny = y+nextY
-            nx = x+nextX
-            if 0<=ny<n and 0<=nx<n and visit[ny][nx]==-1 and matrix[ny][nx]<=shark_size:
-                visit[ny][nx]=visit[y][x]+1
-                queue.append([ny,nx])
-    return visit
-
-def find_find(visit):
-    min_value = 999999999
-    y,x =-1,-1
-    for i in range(n):
-        for j in range(n):
-            if visit[i][j]!=-1 and 0<matrix[i][j]<shark_size:
-                if min_value>visit[i][j]:
-                    min_value=visit[i][j]
-                    y,x =i,j
-    if min_value==999999999:
-        return False
-    else:
-        return y,x,min_value
-
-while True:
-    result = find_find(bfs())
-
-    if not result:
-        print(time)
-        break
-    else:
-        now = [result[0],result[1]]
-        time+=result[2]
-        matrix[result[0]][result[1]]=0
-        eat_fish+=1
-        if eat_fish==shark_size:
-            eat_fish=0
-            shark_size +=1
+for i in range(m):
+    y, x, speed, move, size = map(int, input().split())
+    heapq.heappush(matrix[y - 1][x - 1], [-size, speed, move])
+move = [[], [-1, 0], [1, 0], [0, 1], [0, -1]]
+answer = 0
 
 
-
-
+for x in range(c):
+    find_shark(x)
+    move_shark()
+print(answer)
