f10d24d4881f34056ce4ba4f41f8611b04d746a6
developer-hajun <juni8828@naver.com>
2024-09-03 23:31:56 +0900

    마법사 상어와 복제, 마법사 상어와 비바라기

--- save/풀이.py
+++ save/풀이.py
@@ -1,78 +1,63 @@
 import copy
 
-moves = [
-    [],
-    [(0,1),(1,0),(-1,0),(0,-1)],
-    [(0,1,0,-1),(1,0,-1,0)],
-    [(1,0,0,1),(1,0,0,-1),(-1,0,0,1),(-1,0,0,-1)],
-    [(1,0,-1,0,0,1),(1,0,-1,0,0,-1),(0,1,0,-1,1,0),(0,1,0,-1,-1,0)],
-    [(1,0,-1,0,0,1,0,-1)]
-]
-def move(temp,y,x,move_x,move_y):
-    count = 0
-    value = 0
-    while True:
-        ny = y+(value*move_y)
-        nx = x+(value*move_x)
-        if 0<=nx<m and 0<=ny<n:
-            if temp[ny][nx]==6:
-                break
-            temp[ny][nx]=-1
-        else:
-            break
-        value+=1
-def solve(board,now):
-    global answer
-    if now==len(cam):
-        value = 0
-        for _ in board:
-            value+=_.count(0)
-        answer = min(answer,value)
-        return
-    y,x,cam_value = cam[now]
-    if cam_value==1:
-        for move_y, move_x in moves[cam_value]:
-            temp = copy.deepcopy(board)
-            move(temp,y,x,move_y,move_x)
-            solve(temp,now+1)
-    elif cam_value==2 or cam_value==3:
-        for move_y, move_x,move_y1, move_x1 in moves[cam_value]:
-            temp = copy.deepcopy(board)
-            move(temp, y, x, move_y, move_x)
-            move(temp, y, x, move_y1, move_x1)
-            solve(temp,now+1)
-    elif cam_value==4:
-        for move_y, move_x,move_y1, move_x1,move_y2,move_x2 in moves[cam_value]:
-            temp = copy.deepcopy(board)
-            move(temp, y, x, move_y, move_x)
-            move(temp, y, x, move_y1, move_x1)
-            move(temp, y, x, move_y2, move_x2)
-            solve(temp,now+1)
-    elif cam_value==5:
-        for move_y, move_x,move_y1, move_x1,move_y2,move_x2,move_y3,move_x3 in moves[cam_value]:
-            temp = copy.deepcopy(board)
-            move(temp, y, x, move_y, move_x)
-            move(temp, y, x, move_y1, move_x1)
-            move(temp, y, x, move_y2, move_x2)
-            move(temp, y, x, move_y3, move_x3)
-            solve(temp,now+1)
+n,m = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+move_cloud = [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]]
+cloud = [[0]*n for _ in range(n)]
+cloud[n-1][0]=1
+cloud[n-1][1]=1
+cloud[n-2][0]=1
+cloud[n-2][1]=1
 
+def m_cloud():
+    new_cloud = [[0]*n for _ in range(n)]
+    for y in range(n):
+        for x in range(n):
+            if cloud[y][x]==1:
+                ny = y + (move_cloud[move][0]*count)
+                nx = x + (move_cloud[move][1]*count)
+                if ny < 0:
+                    while not 0<=ny<n:
+                        ny = n-abs(ny)
+                if ny >= n:
+                    ny = ny % n
+                if nx < 0:
+                    while not 0 <= nx < n:
+                        nx = n-abs(nx)
+                if nx >= n:
+                    nx = nx % n
+                new_cloud[ny][nx]=1
+    for y in range(n):
+        for x in range(n):
+            if new_cloud[y][x] == 1:
+                matrix[y][x]+=1
 
+    return new_cloud
 
-
-n,m = map(int,input().split())
-matrix = []
-visit=[[0]*m for _ in range(n)]
-cam = []
-wall=0
-for i in range(n):
-    line = list(map(int,input().split()))
-    matrix.append(line)
-    for j in range(m):
-        if line[j]==6:
-            wall+=1
-        elif line[j]!=0:
-            cam.append([i,j,line[j]])
-answer = 64
-solve(matrix,0)
+def rain_mod():
+    for y in range(n):
+        for x in range(n):
+            if cloud[y][x]:
+                count=0
+                for ny,nx in [y+1,x+1],[y-1,x-1],[y-1,x+1],[y+1,x-1]:
+                    if 0<=ny<n and 0<=nx<n and matrix[ny][nx]:
+                        count+=1
+                matrix[y][x]+=count
+def new_cloud():
+    for y in range(n):
+        for x in range(n):
+            if cloud[y][x]==1:
+                cloud[y][x]=0
+            elif matrix[y][x]>1:
+                matrix[y][x]-=2
+                cloud[y][x]=1
+for _ in range(m):
+    move , count = map(int, input().split())
+    move-=1
+    cloud = m_cloud()
+    rain_mod()
+    new_cloud()
+answer = 0
+for _ in matrix:
+    answer += sum(_)
 print(answer)
