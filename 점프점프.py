aeb3da35ddab62a07e2578004ecd61bc8fdb6d5f
developer-hajun <juni8828@naver.com>
2024-10-30 11:32:06 +0900

    dp연습

--- 점프점프.py
+++ 점프점프.py
@@ -1,11 +1,9 @@
-n = int(input())
-
+n=int(input())
 arr=list(map(int,input().split()))
-
-dp=[1]*n
+dp =[1]*n
 
 for i in range(n):
     for j in range(i):
-        if arr[i]<arr[j]:
+        if arr[i]>arr[j]:
             dp[i]=max(dp[i],dp[j]+1)
-print(n-max(dp))
+print(max(dp))
