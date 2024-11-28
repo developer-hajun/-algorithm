dd61ff0d0dd90712a1856c5924bd58dfd64327b6
developer-hajun <juni8828@naver.com>
2024-11-28 21:08:53 +0900

    프로그래머스 다시 로직 분석후 풀어보기 이모티콘 할인행사,테이블해시함수

--- 이모티콘 할인행사.py
+++ 이모티콘 할인행사.py
@@ -1,25 +1,24 @@
-from itertools import permutations, product
-
-
+from itertools import product
 def solution(users, emoticons):
-    answer = [0, 0]
-    for discount in product([10, 20, 30, 40], repeat=len(emoticons)):
-        plus = 0
-        money = 0
-        for user in users:
-            emoticon_buy = 0
-            for i in range(len(emoticons)):
-                if discount[i] >= user[0]:
-                    emoticon_buy += emoticons[i] * ((100 - discount[i]) / 100)
-
-            if user[1] <= emoticon_buy:
-                plus += 1
+    answer = []
+    for case in product([10,20,30,40],repeat = len(emoticons)):
+        arr = []
+        for i in range(len(emoticons)):
+            value = emoticons[i]-(emoticons[i]*(case[i]/100))
+            arr.append(value)
+        user = [0]*len(users)
+        for i in range(len(arr)):
+            for j in range(len(user)):
+                if case[i]<users[j][0]:
+                    continue
+                user[j]+=arr[i]
+        plus=0
+        count = 0
+        for i in range(len(user)):
+            if user[i]>=users[i][1]:
+                plus+=1
             else:
-                money += emoticon_buy
-        if answer[0] < plus:
-            answer = [plus, int(money)]
-        elif answer[0] == plus:
-            if answer[1] < money:
-                answer = [plus, int(money)]
-
-    return answer
+                count+=user[i]
+        answer.append([plus,count])
+    answer.sort(key=lambda x:[-x[0],-x[1]])
+    return answer[0][0],int(answer[0][1])
--- 테이블 해시 함수.py
+++ 테이블 해시 함수.py
@@ -1,22 +1,12 @@
-def xor_array(arr):
-    result = 0
-    for num in arr:
-        result ^= num  # XOR 연산
-    return result
-
-
 def solution(data, col, row_begin, row_end):
-    data.sort(key=lambda x: (x[col - 1], -x[0]))
-    data = data[row_begin - 1: row_end]
-    now = 0
-
-    value = []
-    for i in range(row_begin, row_end + 1):
-        v = 0
-        for j in data[now]:
-            v += j % i
-        now += 1
-        value.append(v)
-
-    xor_result = xor_array(value)
-    return xor_result
+    data.sort(key = lambda x:[x[col-1],-x[0]])
+    data = data[row_begin-1:row_end]
+    ans = []
+    for i in range(row_begin,row_end+1):
+        value = data[i-row_begin]
+        d = sum(k%i for k in value)
+        ans.append(d)
+    result = 0
+    for i in range(len(ans)):
+        result ^=ans[i]
+    return result
