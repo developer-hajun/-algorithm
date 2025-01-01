3e2a178d0aa65b7bd60ac0e456a7ab083bc21036
developer-hajun <juni8828@naver.com>
2025-01-01 22:01:20 +0900

    뱀

--- save/풀이.py
+++ save/풀이.py
@@ -1,20 +1,28 @@
-def solution(board, moves):
-    stack = []
-    les = len(board[0])
-    answer = 0
-    for pick in moves:
-        pick = pick - 1
-        check = True
-        for i in range(les):
-            if board[i][pick] != 0:
-                stack.append(board[i][pick])
-                check = False
-                board[i][pick] = 0
-                break
-        if check:
+from collections import deque
+
+n = int(input())
+
+sang = []
+sang.append([])
+for i in range(n):
+    a,b = map(int,input().split())
+    sang.append([a,b])
+
+q = deque()
+
+day = [0]*(n+2)
+
+def bosu(a,b):
+    global day
+    for i in range(1,n+1):
+        if i<a or i+sang[i][0]>n+1: #현재 날짜보다 그전에 의뢰이거나,의뢰를 받았을때 n일을 넘으면 패쓰
             continue
-        while len(stack) > 1 and stack[-1] == stack[-2]:
-            stack.pop()
-            stack.pop()
-            answer += 2
-    return answer
+        else:
+            if b+sang[i][1] <= day[i+sang[i][0]]: #내가 상담을 함으로써 받을수 있는 보수보다, 다른 상담으로 받을수 있는 액수가 더 클때
+                continue
+            else:
+                day[i+sang[i][0]] = b+sang[i][1]
+                bosu(i+sang[i][0],b+sang[i][1])
+
+bosu(1,0)#상담받을수 있는날짜,현재보수
+print(max(day))
