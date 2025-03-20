1ddf82aa602628c535be99b2491c40b9a531ad33
developer-hajun <juni8828@naver.com>
2025-03-20 14:41:12 +0900

    순위검색,메뉴리뉴얼

--- save/풀이.py
+++ save/풀이.py
@@ -1,42 +1,31 @@
-def solution(info, edges):
-    graph = [[] for _ in range(len(info))]
-    for a, b in edges:
-        graph[a].append(b)
-    visit = [0] * len(info)
-    visit[0] = -1
-    for i in graph[0]:
-        visit[i] = 1
+def solution(info, query):
+    data = dict()
+    for a in ['cpp', 'java', 'python', '-']:
+        for b in ['backend', 'frontend', '-']:
+            for c in ['junior', 'senior', '-']:
+                for d in ['chicken', 'pizza', '-']:
+                    data.setdefault((a, b, c, d), list())
+    for i in info:
+        i = i.split()
+        for a in [i[0], '-']:
+            for b in [i[1], '-']:
+                for c in [i[2], '-']:
+                    for d in [i[3], '-']:
+                        data[(a, b, c, d)].append(int(i[4]))
+    for k in data:
+        data[k].sort()
+    answer = []
+    for i in query:
+        i = i.split()
+        scores = data[(i[0], i[2], i[4], i[6])]
+        wanted = int(i[7])
 
-    global answer
-    answer = -1
-
-    # : 이미 방문 0: 아직 방문 못함 1: 방문 가능
-    def dfs(sheep, wolf, go):
-        global answer
-        answer = max(answer, sheep)
-        for i in range(len(visit)):
-            if go[i] == 1:
-                if info[i] == 0:
-                    for _ in graph[i]:
-                        if go[_] == 0:
-                            go[_] = 1
-                    go[i] = -1
-                    dfs(sheep + 1, wolf, go)
-                    go[i] = 1
-                    for _ in graph[i]:
-                        if go[_] == 1:
-                            go[_] = 0
-                else:
-                    if sheep > wolf + 1:
-                        for _ in graph[i]:
-                            if go[_] == 0:
-                                go[_] = 1
-                        go[i] = -1
-                        dfs(sheep, wolf + 1, go)
-                        go[i] = 1
-                        for _ in graph[i]:
-                            if go[_] == 1:
-                                go[_] = 0
-
-    dfs(1, 0, visit)
+        start, end = 0, len(scores)
+        while start < end:
+            mid = (start + end) // 2
+            if scores[mid] >= wanted:
+                end = mid
+            else:
+                start = mid + 1
+        answer.append(len(scores) - start)
     return answer
--- 순위검색.py
+++ 순위검색.py
@@ -1,6 +1,3 @@
-import bisect, itertools, collections
-
-
 def solution(info, query):
     data = dict()
     for a in ['cpp', 'java', 'python', '-']:
@@ -8,7 +5,6 @@ def solution(info, query):
             for c in ['junior', 'senior', '-']:
                 for d in ['chicken', 'pizza', '-']:
                     data.setdefault((a, b, c, d), list())
-
     for i in info:
         i = i.split()
         for a in [i[0], '-']:
@@ -16,23 +12,20 @@ def solution(info, query):
                 for c in [i[2], '-']:
                     for d in [i[3], '-']:
                         data[(a, b, c, d)].append(int(i[4]))
-
     for k in data:
         data[k].sort()
+    answer = []
+    for i in query:
+        i = i.split()
+        scores = data[(i[0], i[2], i[4], i[6])]
+        wanted = int(i[7])
 
-    answer = list()
-
-    for q in query:
-        q = q.split()
-        scores = data[(q[0], q[2], q[4], q[6])]
-        wanted = int(q[7])
-
-        l, r = 0, len(scores)
-        while l < r:
-            middle = (l + r) // 2
-            if scores[middle] >= wanted:
-                r = middle
+        start, end = 0, len(scores)
+        while start < end:
+            mid = (start + end) // 2
+            if scores[mid] >= wanted:
+                end = mid
             else:
-                l = middle + 1
-        answer.append(len(scores) - l)
+                start = mid + 1
+        answer.append(len(scores) - start)
     return answer
