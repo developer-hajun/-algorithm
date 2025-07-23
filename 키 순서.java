700307a0e3a656f8725bc16bf8b066112871330e
developer-hajun <juni8828@naver.com>
2025-07-23 22:37:44 +0900

    싸피 코테 연습

--- 키 순서.java
+++ 키 순서.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class Main {
-    static List<Integer>[] up;
-    static List<Integer>[] down;
+    static List<List<Integer>> up;
+    static List<List<Integer>> down;
     static boolean[] visit;
     static int count;
 
     public static void up_count(int now) {
-        for (int next : up[now]) {
+        for (int next : up.get(now)) {
             if (visit[next]) continue;
             visit[next] = true;
             count++;
@@ -16,7 +16,7 @@ class Main {
     }
 
     public static void down_count(int now) {
-        for (int next : down[now]) {
+        for (int next : down.get(now)) {
             if (visit[next]) continue;
             visit[next] = true;
             count++;
@@ -24,25 +24,24 @@ class Main {
         }
     }
 
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
 
-        up = (ArrayList<Integer>[]) new ArrayList[n];
-        down = (ArrayList<Integer>[]) new ArrayList[n];
+        up = new ArrayList<>();
+        down = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            up[i] = new ArrayList<>();
-            down[i] = new ArrayList<>();
+            up.add(new ArrayList<>());
+            down.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; i++) {
             int a = sc.nextInt() - 1;
             int b = sc.nextInt() - 1;
 
-            up[a].add(b);
-            down[b].add(a);
+            up.get(a).add(b);
+            down.get(b).add(a);
         }
 
         int answer = 0;
