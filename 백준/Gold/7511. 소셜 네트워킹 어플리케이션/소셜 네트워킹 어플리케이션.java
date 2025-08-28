f27f50672235c36477b03b89af178565ef798c92
developer-hajun <76621595+developer-hajun@users.noreply.github.com>
2025-08-28 16:44:43 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 1048 ms, Memory: 165036 KB -BaekjoonHub

---
+++ 백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.java
@@ -0,0 +1,72 @@
+
+import java.util.*;
+import java.io.*;
+public class Main {
+	static int[] parents;
+	
+	public static int find_parents(int x) {
+		if(parents[x]!=x) parents[x]=find_parents(parents[x]);
+		return parents[x];
+	}
+	public static void union(int left,int right) {
+		if(left<right) {
+			parents[right]=left;
+		}
+		else {
+			parents[left]=right;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(st.nextToken());
+		for(int tc=1;tc<=t;tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			int m = Integer.parseInt(st.nextToken());
+			Queue<int[]> queue = new ArrayDeque<>();
+			for(int i=1;i<=m;i++) {
+				st = new StringTokenizer(br.readLine());
+				int now = Integer.parseInt(st.nextToken());
+				int now2 = Integer.parseInt(st.nextToken());
+				queue.add(new int[] {now,now2});
+			}
+			parents = new int[n+1];
+			for(int i=0;i<=n;i++) parents[i]=i;
+			
+			long answer = 0L;
+			boolean check = false;
+			while(!queue.isEmpty()) {
+				int[] now = queue.poll();
+				int lp = find_parents(now[0]);
+				int rp = find_parents(now[1]);
+				if(lp!=rp) {
+					union(lp,rp);
+				}
+			}
+			for(int i=0;i<n;i++) {
+				find_parents(i);
+			}
+			sb.append("Scenario ").append(tc).append(":\n");
+			st = new StringTokenizer(br.readLine());
+			int k = Integer.parseInt(st.nextToken());
+			for(int i=1;i<=k;i++) {
+				st = new StringTokenizer(br.readLine());
+				int now = Integer.parseInt(st.nextToken());
+				int now2 = Integer.parseInt(st.nextToken());
+				if(parents[now]==parents[now2]) {
+					sb.append(1).append("\n");
+				}
+				else {
+					sb.append(0).append("\n");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+
+}
