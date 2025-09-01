619d78098ee068ff198e2802339edf47eb72b302
developer-hajun <76621595+developer-hajun@users.noreply.github.com>
2025-09-01 16:34:44 +0900

    [Gold IV] Title: 사이클 게임, Time: 592 ms, Memory: 123816 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,56 +1,43 @@
-
 import java.util.*;
 import java.io.*;
+
 public class Main {
-	static int[] parents;
-	
-	public static int find_parents(int x) {
-		if(parents[x]!=x) parents[x]=find_parents(parents[x]);
-		return parents[x];
-	}
-	public static void union(int left,int right) {
-		if(left<right) {
-			parents[right]=left;
-		}
-		else {
-			parents[left]=right;
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	static int[] parent;
+	public static int find_parent(int x){
+		if(parent[x]!=x) parent[x]=find_parent(parent[x]);
+		return parent[x];
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		Queue<int[]> queue = new ArrayDeque<>();
-		for(int i=1;i<=m;i++) {
-			st = new StringTokenizer(br.readLine());
-			int now = Integer.parseInt(st.nextToken());
-			int now2 = Integer.parseInt(st.nextToken());
-			queue.add(new int[] {now,now2});
-		}
-		parents = new int[n+1];
-		for(int i=0;i<=n;i++) parents[i]=i;
-		
-		long answer = 0L;
-		boolean check = false;
-		while(!queue.isEmpty()) {
-			int[] now = queue.poll();
-			int lp = find_parents(now[0]);
-			int rp = find_parents(now[1]);
-			if(lp!=rp) {
-				union(lp,rp);
-				answer++;
-			}
-			else {
-				answer++;
-				check = true;
-				break;
-			}
-		}
-		answer = check ? answer : 0L;
-		System.out.println(answer);
 	}
+	public static void union_parent(int lp,int rp) {
+		if(lp<rp) parent[rp]=lp;
+		else parent[lp]=rp;
+	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        parent = new int[v+1];
+        int answer = 0;
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1,o2)->o1[2]-o2[2]);
+        for(int i=1;i<=v;i++) parent[i]=i;
+        for(int i=1;i<=e;i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int a = Integer.parseInt(st.nextToken());
+        	int b = Integer.parseInt(st.nextToken());
+        	int lp = find_parent(a);
+        	int rp = find_parent(b);
+        	if(lp!=rp) {
+        		union_parent(lp,rp);
+        	}
+        	else {
+        		System.out.println(i);
+        		return;
+        	}
+        }
+        System.out.print(0);
+        
+        
 
+    }
 }
