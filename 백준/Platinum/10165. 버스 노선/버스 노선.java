e838bcfe3e873edbdb8ec2d25c9eeb8660cad2bc
developer-hajun <76621595+developer-hajun@users.noreply.github.com>
2025-10-27 14:55:33 +0900

    [Platinum V] Title: 버스 노선, Time: 2108 ms, Memory: 211832 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/10165. 버스 노선/버스 노선.java
+++ 백준/Platinum/10165. 버스 노선/버스 노선.java
@@ -2,49 +2,54 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-
 class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        long n = Long.parseLong(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] buslist = new int[m*2][3];
+        long[][] buslist = new long[m * 2][3];
 
-        for(int i=0;i<m;i++){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            if(a>b){
-                b = b+n;
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            
+            if (a > b) {
+                b = b + n;
             }
-            buslist[i*2] = new int[]{a,b,i};
-            buslist[i*2+1] = new int[]{a+n,b+n,i};
+            buslist[i * 2] = new long[]{a, b, i};
+            buslist[i * 2 + 1] = new long[]{a + n, b + n, i};
         }
 
-        Arrays.sort(buslist,(o1,o2)-> {
-            
-            if(o1[0]==o2[0]) return o2[1]-o1[1];
-            return o1[0]-o2[0];});
-        boolean[] cancle = new boolean[m];
-        int max_end = -1;
-
-        for(int i=0;i<m*2;i++){
-            if(buslist[i][1] <= max_end){
-                cancle[buslist[i][2]] = true;
+        Arrays.sort(buslist, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Long.compare(o2[1], o1[1]); 
             }
-            else{
+            return Long.compare(o1[0], o2[0]);
+        });
+        
+        boolean[] cancle = new boolean[m];
+        
+        long max_end = -1L; 
+
+        for (int i = 0; i < m * 2; i++) {
+            if (buslist[i][1] <= max_end) {
+                cancle[(int)buslist[i][2]] = true;
+            } else {
                 max_end = buslist[i][1];
             }
         }
 
-        for(int i=0;i<m;i++){
-            if(cancle[i]) continue;
-            System.out.print((i+1)+" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if (cancle[i]) continue;
+            sb.append(i + 1).append(" ");
         }
+        System.out.println(sb.toString().trim());
     }
 }
