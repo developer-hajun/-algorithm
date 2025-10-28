cbcceb84ec8ba66dd81b34fd7e903dfae2635112
developer-hajun <76621595+developer-hajun@users.noreply.github.com>
2025-10-28 11:25:14 +0900

    [Gold V] Title: 암호 만들기, Time: 200 ms, Memory: 18160 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static char[] arr;
+    static char[] use;
+    static int l,c;
+    static String word = "aeiou";
+    public static void dfs(int ja,int mo,int next,int count){
+        if(count==l){
+            if(mo<1 || ja<2 ) return;
+            System.out.println(new String(arr));
+            return;
+        }
+        for(int i=next;i<use.length;i++){
+            char index = use[i];
+            arr[count] = index;
+            if(word.indexOf(index)!=-1){
+                dfs(ja,mo+1,i+1,count+1);
+            }
+            else{
+                dfs(ja+1,mo,i+1,count+1);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        l=sc.nextInt();c=sc.nextInt();
+        arr = new char[l];
+        use = new char[c];
+        for(int i=0;i<c;i++) use[i] = sc.next().charAt(0);
+        Arrays.sort(use);
+        dfs(0,0,0,0);//자음개수,모음개수,다음위치,카운트
+        
+    }
+}
