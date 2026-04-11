91b0691be84a64727024a0a2e2812bbd7f30222a
developer-hajun <76621595+developer-hajun@users.noreply.github.com>
2026-04-11 10:34:51 +0900

    [level 3] Title: 이중우선순위큐, Time: 79.52 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,18 +1,23 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] operations) {
-        TreeSet<Integer> answer = new TreeSet<>();
-        for(String now : operations){
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (String now : operations) {
             String[] co = now.split(" ");
             char command = co[0].charAt(0);
             int num = Integer.parseInt(co[1]);
-            if(command=='I') answer.add(num);
-            else if(answer.size()==0) continue;
-            else if(num==1) answer.pollLast();
-            else answer.pollFirst();
+
+            if (command == 'I') {
+                map.merge(num, 1, Integer::sum);
+            } else if (map.isEmpty()) {
+                continue;
+            } else {
+                int key = (num == 1) ? map.lastKey() : map.firstKey();
+                if (map.get(key) == 1) map.remove(key);
+                else map.put(key,map.get(key)-1);
+            }
         }
-        if(answer.size()==0) return new int[]{0,0};
-        return new int[]{answer.last(),answer.first()};
+        if (map.isEmpty()) return new int[]{0, 0};
+        return new int[]{map.lastKey(), map.firstKey()};
     }
 }
