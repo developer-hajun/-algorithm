da1d8e0332b096fce82c7fbc0033594ccbcd51f1
developer-hajun <76621595+developer-hajun@users.noreply.github.com>
2026-04-11 10:21:26 +0900

    [level 3] Title: 순위, Time: 32.62 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,25 +1,49 @@
+import java.util.*;
 class Solution {
+    
+    Set<Integer>[][] winner;
+    
     public int solution(int n, int[][] results) {
-        boolean[][] graph = new boolean[n + 1][n + 1];
-
-        for (int[] r : results) {
-            graph[r[0]][r[1]] = true;
+        winner = new HashSet[2][n+1];
+        List<Integer> move[] = new ArrayList[n+1];
+        boolean[] visit = new boolean[n+1];
+        for(int i=0;i<=n;i++){
+            move[i] = new ArrayList<>();
+            winner[0][i] = new HashSet<>();
+            winner[1][i] = new HashSet<>();
+        }
+        for(int[] r : results) move[r[0]].add(r[1]);
+        for(int i=1;i<=n;i++){
+            if(visit[i]) continue;
+            dfs(move,visit,i,0);
+        }
+        move = new ArrayList[n+1];
+        visit = new boolean[n+1];
+        for(int i=0;i<=n;i++) move[i] = new ArrayList<>();
+        for(int[] r : results) move[r[1]].add(r[0]);
+        for(int i=0;i<=n;i++){
+            if(visit[i]) continue;
+            dfs(move,visit,i,1);
+        }
+        int answer=0;
+        for(int i=1;i<=n;i++){
+            if(winner[0][i].size()+winner[1][i].size()==n-1) answer++;
         }
         
-        for (int k = 1; k <= n; k++)
-            for (int i = 1; i <= n; i++)
-                for (int j = 1; j <= n; j++)
-                    if (graph[i][k] && graph[k][j])
-                        graph[i][j] = true;
-
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            int known = 0;
-            for (int j = 1; j <= n; j++) {
-                if (graph[i][j] || graph[j][i]) known++;
-            }
-            if (known == n - 1) answer++;
-        }
+        
         return answer;
     }
+    
+    public Set<Integer> dfs(List<Integer>[] move,boolean[] visit,int now,int w){
+        if(visit[now]) return winner[w][now];
+        visit[now]=true;
+        for(int next:move[now]){
+            winner[w][now].addAll(dfs(move,visit,next,w));
+            winner[w][now].add(next);
+        }
+        return winner[w][now];
+        
+        
+    }
+    
 }
